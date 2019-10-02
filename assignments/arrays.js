88daf9181db8e0302dd6608562a61c792895b55d
Christie <cpam2011@yahoo.com>
2019-10-02 15:22:27 -0400

    updated someinfo

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -86,8 +86,8 @@ for (let i = 32; i < 33; i++) {
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-let lastCar = 
-console.log(`${inventory[49].car_make} ${inventory[49].car_model}`);
+let lastCar = inventory[inventory.length-1];
+console.log(`The last car in the inventory list is a ${lastCar.car_make} ${lastCar.car_model}`);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
